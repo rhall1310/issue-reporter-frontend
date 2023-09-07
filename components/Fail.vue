f2ce5c6b248bd67336f66d37d42115f5e198f024
Rob Hall <r-narch@hotmail.com>
2023-09-07 16:15:25 +0100

    error component

---
+++ components/Fail.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container" id="fail">
+    <h1 class="text-danger" id="error">Something went wrong</h1>
+    <a href="/">
+      <button type="button" class="btn btn-primary btn-lg" id="retry">
+        Try again
+      </button>
+    </a>
+  </div>
+</template>
--- pages/success.vue
+++ pages/success.vue
@@ -28,14 +28,7 @@
         </button>
       </a>
     </div>
-    <div class="container" id="fail" v-else>
-      <h1 class="text-danger" id="error">Something went wrong</h1>
-      <a href="/">
-        <button type="button" class="btn btn-primary btn-lg" id="retry">
-          Try again
-        </button>
-      </a>
-    </div>
+    <Fail v-else />
   </div>
 </template>
 
