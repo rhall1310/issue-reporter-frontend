e1039d26b0f2f4ddb5239ff38f1ea0be97b32471
Rob Hall <r-narch@hotmail.com>
2023-09-18 12:33:09 +0100

    reported map

--- pages/reported.vue
+++ pages/reported.vue
@@ -1,5 +1,11 @@
 <template>
-  <h2>See what's already been reported</h2>
+  <div>
+    <Header />
+    <h2>See what's already been reported</h2>
+    <Map />
+
+    <a href="/"><p>Report something yourself</p></a>
+  </div>
 </template>
 
 <script>
