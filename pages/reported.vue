a02a2a94159863c9c92b2f2b75d15fcd926e96cd
Rob Hall <r-narch@hotmail.com>
2023-09-19 18:11:50 +0100

    report details pointer

--- pages/reported.vue
+++ pages/reported.vue
@@ -3,6 +3,7 @@
     <Header />
     <h2>See what's already been reported</h2>
     <Map />
+    <p>Click on a marker to see the report details</p>
 
     <a href="/"><p>Report something yourself</p></a>
   </div>
