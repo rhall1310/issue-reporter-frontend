c23856fab4f2aa5f002799edf78ef15fe966272a
Rob Hall <r-narch@hotmail.com>
2024-08-07 16:24:37 +0100

    already reported header

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="header bg-info text-light">
-    <h5></h5>
+    <a href="/reported" class="already text-light">
+      <h5>Or see what's already been reported</h5></a
+    >
     <h1>Report a problem</h1>
   </div>
 </template>
@@ -11,7 +13,14 @@ export default {};
 
 <style>
 .header {
+  display: flex;
+  justify-content: center;
   text-align: center;
   padding: 0.3em;
+}
+.already {
+  position: absolute;
+  right: 1em;
+  text-decoration: none;
 }
 </style>
